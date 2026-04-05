90cdece15cc03dde87739e3f41144062aa81ca7c
anarchie <116115028+anarchie347@users.noreply.github.com>
2026-04-05 18:12:12 +0100

    completed initial cloudfront and s3 iac

--- deployment/cloudfront.tf
+++ deployment/cloudfront.tf
@@ -5,25 +5,15 @@ resource "aws_cloudfront_origin_access_control" "s3-access" {
   signing_protocol = "sigv4"
 }
 
-# resource "aws_cloudfront_cache_policy" "default" {
-#   name = "default"
-#   min_ttl = 1
-#   max_ttl = 500
-#   default_ttl = 50
-#   parameters_in_cache_key_and_forwarded_to_origin {
-    
-#   }
-# }
-
 resource "aws_cloudfront_distribution" "main" {
   enabled = true
   is_ipv6_enabled = true
   default_root_object = "index.html"
-
   origin {
     domain_name = aws_s3_bucket.mainpage.bucket_regional_domain_name
     origin_access_control_id = aws_cloudfront_origin_access_control.s3-access.id
     origin_id = "Mainpage S3"
+    origin_path = "/public"
   }
 
   default_cache_behavior {
@@ -33,6 +23,7 @@ resource "aws_cloudfront_distribution" "main" {
     viewer_protocol_policy = "redirect-to-https"
     allowed_methods = ["GET", "HEAD"]
   cached_methods = ["GET", "HEAD"]
+  compress = true
   }
   restrictions {
     geo_restriction {
--- deployment/s3_mainpage.tf
+++ deployment/s3_mainpage.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket" "mainpage" {
 // Policy for allowing CloudFront access to the bucket
 data "aws_iam_policy_document" "origin_mainpage_bucket_policy" {
   statement {
-    sid = "AllowCloudFrontServicePrincipalReadWrite"
+    sid = "AllowCloudFrontServicePrincipal"
     effect = "Allow"
 
     principals {
@@ -19,15 +19,14 @@ data "aws_iam_policy_document" "origin_mainpage_bucket_policy" {
 
     actions = [
         "s3:GetObject",
-        "s3:PutObject"
     ]
 
     resources = [
-        "${aws_s3_bucket.mainpage.arn}/dist/*"
+        "${aws_s3_bucket.mainpage.arn}/*"
     ]
 
     condition {
-      test = "StringEquals"
+      test = "ArnLike"
       variable = "AWS:SourceArn"
       values = [aws_cloudfront_distribution.main.arn]
     }
@@ -40,3 +39,22 @@ resource "aws_s3_bucket_policy" "mainpage_policy" {
   policy = data.aws_iam_policy_document.origin_mainpage_bucket_policy.json
 }
 
+
+locals {
+  upload_folder = "${path.root}/../src/s3/mainpage/dist/"
+  // Files uploaded through terraform dont correctly configure the metadata type
+  // https://stackoverflow.com/questions/18296875/amazon-s3-downloads-index-html-instead-of-serving
+  // may need to add ore types in the future
+  mime_types = {
+    ".html" = "text/html"
+    ".css" = "text/css"
+    ".js" = "application/javascript"
+  }
+}
+resource "aws_s3_object" "name" {
+  for_each = fileset(local.upload_folder, "**")
+  bucket = aws_s3_bucket.mainpage.bucket
+  key = "public/${each.value}"
+  source = "${local.upload_folder}/${each.value}"
+  content_type = lookup(local.mime_types,regex("\\.[^.]+$", each.value), "application/octet-stream")
+}
