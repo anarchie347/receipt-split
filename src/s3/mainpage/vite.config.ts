e2df6af50910ea8a389b8bc27d1c99ccebcd44be
anarchie <116115028+anarchie347@users.noreply.github.com>
2026-04-12 19:10:49 +0100

    added proxying during local development

--- src/s3/mainpage/vite.config.ts
+++ src/s3/mainpage/vite.config.ts
@@ -1,15 +1,27 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      react: path.resolve("./node_modules/react"),
-      "react-dom": path.resolve("./node_modules/react-dom"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        react: path.resolve("./node_modules/react"),
+        "react-dom": path.resolve("./node_modules/react-dom"),
+      },
     },
-  },
+    server: {
+      proxy: {
+        "/api": {
+          target: env.DEBUG_TARGET,
+          changeOrigin: true,
+          secure: true,
+        },
+      },
+    },
+  };
 });
